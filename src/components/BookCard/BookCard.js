b071200922ba84a9654d890e1d658c47c91b9765
unknown <amirsaman.gord@gmail.com>
2019-05-09 13:05:05 +0430

    working on some stylings

--- src/components/BookCard/BookCard.js
+++ src/components/BookCard/BookCard.js
@@ -1,11 +1,19 @@
 import React from 'react';
 import classes from './BookCard.module.scss';
 
+import bookImg from '../../assets/book2.jpg';
+
 const BookCard = props => {
   return (      
     <div className={classes.BookCard} onClick={props.selected}>
-      <p><strong>Title</strong> : {props.name}</p>
-      <p><strong>Author</strong> : {props.author}</p>
+      <div className={classes.BookCard__imgPlaceHolder}>
+        <img src={bookImg}/>
+      </div>
+      <div className={classes.BookCard__content}>
+        <p><strong>{props.name}</strong></p>
+        <p><strong>{props.author}</strong></p>
+        <p><strong>{props.price}$</strong></p>
+      </div>
     </div>
   );
 };
--- src/containers/Home/Home.js
+++ src/containers/Home/Home.js
@@ -12,6 +12,16 @@ import Quantity from '../../components/Quantity/Quantity';
 
 import Loading from '../../components/UI/Loading/Loading';
 
+import bgImg from '../../assets/book1.jpg';
+
+const bgStyles = {  
+  backgroundImage: `url(${bgImg})`
+};
+
+const testBtnStyles = {
+  borderColor: 'orange'
+}
+
 class Home extends Component {
 
   state = {
@@ -63,21 +73,25 @@ class Home extends Component {
     if(this.props.selectedBook !== null){
       modal = (
         <Modal closeModal={this.closeModalHandler}>
+          <div className={classes.PreviewModal} style={bgStyles}>
 
-          <p><strong>Title</strong> : {this.props.books[this.props.selectedBook].title}</p>
-          <p><strong>Author</strong> : {this.props.books[this.props.selectedBook].author}</p>
-          <p><strong>Country</strong> : {this.props.books[this.props.selectedBook].country}</p>
-          <p><strong>Language</strong> : {this.props.books[this.props.selectedBook].language}</p>
-          <p><strong>Year</strong> : {this.props.books[this.props.selectedBook].year}</p>
+            <div className={classes.PreviewModal__bookInfo}>
+              <p><strong>Title</strong> : {this.props.books[this.props.selectedBook].title}</p>
+              <p><strong>Author</strong> : {this.props.books[this.props.selectedBook].author}</p>
+              <p><strong>Country</strong> : {this.props.books[this.props.selectedBook].country}</p>
+              <p><strong>Language</strong> : {this.props.books[this.props.selectedBook].language}</p>
+              <p><strong>Year</strong> : {this.props.books[this.props.selectedBook].year}</p>
+            </div>
 
-          <div className={classes.addToCartFunc}>
+            <div className={classes.addToCartFunc}>
 
-            <h3><strong>PRICE</strong> : {this.props.books[this.props.selectedBook].price}</h3>
-            <Quantity qNum={this.state.qNum} addQuantity={this.addQuantityHandler} subQuantity={this.subQuantityHandler}/>            
-            <Button btnType="Button--Success" clicked={() => this.addToCartHandler({item: this.props.books[this.props.selectedBook], number: this.state.qNum})}>add to cart</Button>
+              <h3><strong>PRICE</strong> : {this.props.books[this.props.selectedBook].price}</h3>
+              <Quantity qNum={this.state.qNum} addQuantity={this.addQuantityHandler} subQuantity={this.subQuantityHandler}/>            
+              <Button customStyles={{fontSize: 30, color: "white"}} clicked={() => this.addToCartHandler({item: this.props.books[this.props.selectedBook], number: this.state.qNum})}>add to cart</Button>
+
+            </div>
 
           </div>
-
         </Modal>
       )
     }
@@ -93,6 +107,7 @@ class Home extends Component {
                       key={el.title} 
                       name={el.title}
                       author={el.author}
+                      price={el.price}
                       />
                       ) : <Loading />
                 }
