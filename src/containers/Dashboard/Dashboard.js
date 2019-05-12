1f38bc37296b7a0ec9c8c0090000acc4724b37ab
unknown <amirsaman.gord@gmail.com>
2019-05-12 15:59:42 +0430

    write basic orders component and write the functionality of getting users order list from database inside Dashboard components CDM

--- src/containers/Dashboard/Dashboard.js
+++ src/containers/Dashboard/Dashboard.js
@@ -4,13 +4,51 @@ import * as actionCreators from '../../store/actions/index';
 
 import classes from './Dashboard.module.scss';
 
+// import * as firebase from 'firebase';
+import db from '../../configFirebase';
+
+// import components
+import Orders from './Orders/Orders';
+
 class Dashboard extends Component {
 
     componentDidMount() {
         if(this.props.userId){
             this.props.onTryAutoSignup();
         }
+        
+        // SET USER INFO START  
+        
+        const ordersRef = db.ref('orders');
+
+        const currentUserId = this.props.userId;
+
+        const anotherOrdersRef = ordersRef.child('user').equalTo(currentUserId);
+
+        ordersRef.on('value', function(snapshot){
+
+            const relatedData = snapshot.val();        
+            let keysArray = Object.keys(relatedData);            
+
+            let userPurchases = [];            
+
+            keysArray.forEach(orderId => {                                                
+                if(currentUserId === relatedData[orderId].user.userId){
+                    userPurchases.push(relatedData[orderId]);
+                }
+            });
+
+            console.log('userPurchases ------> ', userPurchases);
+            
+        })
+                
+
+        console.log('anotherOrdersRef ---> ',anotherOrdersRef);
+        
+        // SET USER INFO END
+
     }
+    
     
 
     render() {
@@ -28,7 +66,9 @@ class Dashboard extends Component {
                             <li>اجاره ها</li>
                         </ul>                        
                     </div>
-                    <div className={classes.dashboard__main}>main</div>                    
+                    <div className={classes.dashboard__main}>
+                        <Orders />
+                    </div>                    
                 </div>
             </div>
         );
---
+++ src/containers/Dashboard/Orders/Orders.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Orders = () => {
+    return (
+        <div>
+            all the orders should be listed here
+        </div>
+    );
+};
+
+export default Orders;
